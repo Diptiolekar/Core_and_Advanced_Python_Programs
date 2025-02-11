084fc33ceb1f91137132b524299cdb8925b698bf
Dipti Olekar <132467551+Diptiolekar@users.noreply.github.com>
2025-02-11 13:08:30 +0530

    Add files via upload

---
+++ Day2_Assignment1.py.py
@@ -0,0 +1,17 @@
+# 1. Calculate the multiplication and sum of two numbers
+# enter the first number
+num1=float(input("enter the first number: "))
+# enter the second number
+num2=float(input("enter the second number: "))
+# perform the operations
+add=num1+num2
+mult=num1*num2
+# print the output
+print("addition:",add)
+print("multiplication:",mult)
+
+'''  OUTPUT
+enter the first number: 4
+enter the second number: 5
+addition: 9.0
+multiplication: 20.0   '''
---
+++ Day2_Assignment2.py.py
@@ -0,0 +1,12 @@
+# 2. Declare two variables and print that which variable is largest
+#     using ternary operators
+# Declare two variables
+num1 = 10
+num2 = 20
+# Use ternary operator to find the largest number
+largest = num1 if num1 > num2 else num2
+
+# Print the result
+print(f"The largest number is: {largest}")
+'''  OUTPUT
+The largest number is: 20  '''
